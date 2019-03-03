b06ef0b716262e10d4f7470bf4066943050fc64b
Piotr Kedziora <piotrkedziorapl@gmail.com>
2019-03-03 09:54:55 +0100

    Add hotspot

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 $(function(){
     $('#hamburger').click(function(){
         $('.nav__menu').slideToggle();
@@ -43,4 +44,94 @@ $(function(){
         $('.slider__progress').css('width', '0px');
     }
     animateProgressBar();
+    //hotspot
+    var arrowsSet = $('.hotspot__arrows');
+    var areas = $('.hotspot__area');
+    var mySlicks = [
+        {
+            anchor: $('#hot-slider .hotspot__list'),
+            arrows: $('#hot-arrows'),
+            arrowNext: $('#hot-next'),
+            arrowPrev: $('#hot-prev'),
+            init: false
+        },
+        {
+            anchor: $('#designer-slider .hotspot__list'),
+            arrows: $('#designer-arrows'),
+            arrowNext: $('#designer-next'),
+            arrowPrev: $('#designer-prev'),
+            init: false
+        },
+        {
+            anchor: $('#featured-slider .hotspot__list'),
+            arrows: $('#featured-arrows'),
+            arrowNext: $('#featured-next'),
+            arrowPrev: $('#featured-prev'),
+            init: false
+        },
+        {
+            anchor: $('#latest-slider .hotspot__list'),
+            arrows: $('#latest-arrows'),
+            arrowNext: $('#latest-next'),
+            arrowPrev: $('#latest-prev'),
+            init: false
+        }
+    ]
+    var titles = $('.hotspot__title');
+    titles.click(function(){
+        var elemIndex = $(this).index();
+        titles.each(function(){
+            $(this).removeClass('hotspot__title--active');
+        });
+        $(this).addClass('hotspot__title--active');
+        areas.each(function(){
+            if(!$(this).hasClass('hide')) $(this).addClass('hide');
+        });
+        mySlicks.forEach(function(el){
+            if (el.init) {
+                el.init = false;
+                el.anchor.slick('unslick');
+            }
+        });
+        arrowsSet.each(function(){
+            if (!$(this).hasClass('hotspot__arrows--hide')) $(this).addClass('hotspot__arrows--hide');
+        });
+        arrowsSet.eq(elemIndex).removeClass('hotspot__arrows--hide');
+        areas.eq(elemIndex).removeClass('hide');
+        if (!mySlicks[elemIndex].init) initSlick(mySlicks[elemIndex]);
+
+    });
+    function initSlick(ob) {
+        ob.anchor.slick({
+        slidesToShow: 5,
+        nextArrow: ob.arrowNext,
+        prevArrow: ob.arrowPrev,
+        centerMode: true,
+        responsive: [
+            {
+              breakpoint: 992,
+              settings: {
+                slidesToShow: 3,
+                centerMode: false,
+              }
+            },
+            {
+              breakpoint: 786,
+              settings: {
+                slidesToShow: 2,
+                centerMode: false,
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                centerMode: false,
+              }
+            }
+          ]
+        });
+        ob.init = true;
+    }
+    initSlick(mySlicks[0]);
 });
